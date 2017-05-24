6649b770788fd06abae35e348ac0cf2469f9356b
Unknown <nhernandezosorto@gmail.com>
2017-05-24 17:30:15 -0600

    Tarea #2
    SSubir Informacion Personal y una foto

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/contactanos', function(req, res, next) {
+  res.render('contactanos', { img: 'images/imagen.jpg' });
+});
 
 module.exports = router;

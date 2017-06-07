fdfff688b98b3d0644a4bfea7385e10f189fe892
Unknown <nhernandezosorto@gmail.com>
2017-06-07 13:38:05 -0600

    hecha la tarea

--- routes/index.js
+++ routes/index.js
@@ -108,34 +108,37 @@ router.post('/principal',function(req,res,next){
        res.status(200).json(Diccionario[req.params.dictionaryKey]);
     });
 
-router.PUT('/api/dictionary/:dictionaryKey/update/:word', function(req,res,next){
-  var newWord= Object.assign({},palabraTemplate,req,body);
-  var _dictionary = req.params.dictionaryKey;
-  var word = req.params.word;
-  Diccionario[_dictionary]= Diccionario[_dictionary].map(function(current_word,i){
-    if(current_word.word===_word){
-current_word = Object,assign(current_word,newWord);
 
-    }
-    return current_word;
+router.put('/api/dictionary/:dictionaryKey/update/:word', function(req,res,next){
+      var newWord = Object.assign({},palabraTemplate,req.body);
+      var _dictionary = req.params.dictionaryKey;
+      var _word = req.params.word;
+      Diccionario[_dictionary] = Diccionario[_dictionary].map(function(current_word,i){
+        if(current_word.word===_word){
+          current_word = Object.assign(current_word, newWord);
+        }
+        return current_word;
+      });
+
+      res.status(200).json(Diccionario[_dictionary]);
   });
-});
+
 //map devuelto un arreglo de todos los valores devueltos
-router.DELETE('/api/dictionary/:dictionaryKey/delete/:word', function(req,res,next){
-  var newWord= Object.assign({},palabraTemplate,req,body);
-  var _dictionary = req.params.dictionaryKey;
-  var word = req.params.word;
-  Diccionario[_dictionary]= Diccionario[_dictionary].map(function(current_word,i){
-    if(current_word.word===_word){
 
-    }else {
-    return current_word;
+     router.delete('/api/dictionary/:dictionaryKey/delete/:word', function(req,res,next){
+       var _dictionary = req.params.dictionaryKey;
+       var _word = req.params.word;
+       Diccionario[_dictionary] = Diccionario[_dictionary].map(function(current_word,i){
+         if(current_word.word===_word){
 
-    }
+         }else{
+             return current_word;
+         }
 
-  });
-     res.status(200).json(Diccionario[_dictionary]);
-});
+       });
+ 
+       res.status(200).json(Diccionario[_dictionary]);
+     });
 //200 todo esta bien
 //300 hay algo en bufer
 //400 no se encontro nada de lo que quiere
